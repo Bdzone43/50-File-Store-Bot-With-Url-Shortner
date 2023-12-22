f636e53c917c159aa7395e6ffadf70bbb62d0a2b
Bdzone43 <150906861+Bdzone43@users.noreply.github.com>
2023-12-22 12:40:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "6782613381"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://SUFYAN33:<SUFYAN33>@cluster0.xpmj7no.mongodb.net/?retryWrites=true&w=majority")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002064315059"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
